1e630a91f3f9d0a7f1bdcf5bc9e91aaa86d354fe
Abdullah Raghib <103993447+abdullah-vidyamantra@users.noreply.github.com>
2022-04-28 18:53:25 +0530

    df

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <suspense>
+    <Loader />
+    <template #fallback>
+      Loading...
+    </template>
+  </suspense>
+</template>
+
+<script>
+import Loader from "./components/Loader.vue";
+
+export default {
+  name: "App",
+  components: {
+    Loader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Hello From Loader</h1>
+  {{ user }}
+</template>
+
+<script>
+export default{
+
+    async setup(){
+    
+        const promise = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve("success");
+          }, 2000);
+        });
+        const user = await promise;
+        return {
+          user,
+        };
+    }
+}
+</script>
+
+<style>
+</style>
